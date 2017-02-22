16e10c4b034b77f966ab1052f4b97efc390f56d2
virtualpatterns <code.virtualpatterns@gmail.com>
2017-02-21 20:50:03 -0500

    Updating for reference from nessa

--- source/server/index.js
+++ source/server/index.js
@@ -5,15 +5,14 @@ import Package from '../package.json'
 
 const LOG_PATH = Path.join(__dirname, '..', 'process', 'logs', `${Package.name}.server.log`)
 const PORT = 8082
-const REGEXP_MOCHA = /^\/www\/vendor\/mocha\/(.*)$/
 const REGEXP_STATIC = /^\/www\/(.*)$/
 
 FileSystem.mkdirp.sync(Path.dirname(LOG_PATH))
 
-Log.clear()
+// Log.clear()
 Log.addConsole()
 Log.addFile(LOG_PATH)
-Log.line()
+// Log.line()
 
 let server = Server.createServer()
 
@@ -41,14 +40,6 @@ server.get('/', (request, response, next) => {
 
 server.get('/www', (request, response, next) => {
   response.redirect('/www/index.html', next)
-})
-
-server.get(REGEXP_MOCHA, (request, response, next) => {
-  Server.serveStatic({
-    'directory': Path.join(__dirname, '..', 'node_modules', 'mocha'),
-    'file': request.params[0],
-    'maxAge': 0
-  })(request, response, next)
 })
 
 server.get(REGEXP_STATIC, (request, response, next) => {
